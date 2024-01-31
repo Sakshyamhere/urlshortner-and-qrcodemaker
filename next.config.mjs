63a3f212ed32ed4b130bd75d7be84a2561cc9d39
Sakshyamhere <aryalsakshyam33@gmail.com>
2024-01-31 21:20:05 +0545

    extra edits

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   env: {
     DB_URI: "mongodb+srv://aryalsakshyam33:iyg98Ct4Wjcshje9@urlshortner.wiljrhg.mongodb.net/?retryWrites=true&w=majority",
     REG : "SaKAReyIxXP"
